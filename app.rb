0a9a0a89532943ec0d50557076295ead2dcbe73f
David Wittman <david@wittman.com>
2012-08-29 22:20:31 -0500

    Use Dalli::Client#fetch

--- app.rb
+++ app.rb
@@ -24,26 +24,13 @@ private
 
 def all_awws
   key = ENV['CF_CONTAINER']
-  cache = connect_to_memcached
 
-  cached_value = cache.get key
-
-  if cached_value.nil?
-    puts "Cached value not found. Issuing Cloud Files API request."
-    container = connect_to_cloud_files.container(ENV['CF_CONTAINER'])
-    result = container.objects().map { |obj| container.object(obj) }
-
-    cache.set key, result
-  else
-    result = cached_value
+  connect_to_memcached.fetch key, do
+    container = CloudFiles::Connection.new(:username => ENV['CF_USERNAME'],
+                                           :api_key  => ENV['CF_APIKEY'])
+                                      .container(ENV['CF_CONTAINER'])
+    container.objects().map { |obj| container.object(obj) }
   end
-
-  return result
-end
-
-def connect_to_cloud_files
-  CloudFiles::Connection.new(:username => ENV['CF_USERNAME'],
-                             :api_key => ENV['CF_APIKEY'])
 end
 
 def connect_to_memcached
