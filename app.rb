d1a139ee6c5a893b50f14819e5afc15f2b8e6eea
David Wittman <david.wittman@rackspace.com>
2012-10-04 15:15:52 -0500

    Cache the URL of the object, not the object
    Rather than iterating over each object and calling public_uri, we
    just grab the container CDN url and store it with the object name.
    This should save tons of CF operations and increase performance by
    about a bajillionty or two.

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ require 'json'
 set :expires_ttl, 21600
 
 get '/' do
-  "<img src=\"#{all_awws.sample.public_url}\" />"
+  "<img src=\"#{all_awws.sample}\" />"
 end
 
 get '/bomb/:count' do |count|
   awws = all_awws
   number_of_pictures_to_return = [awws.count, count.to_i].min
-  awws.shuffle[1..number_of_pictures_to_return].map(&:public_url).to_json
+  awws.shuffle[1..number_of_pictures_to_return].to_json
 end
 
 get '/random' do
@@ -29,7 +29,7 @@ def all_awws
     container = CloudFiles::Connection.new(:username => ENV['CF_USERNAME'],
                                            :api_key  => ENV['CF_APIKEY'])
                                       .container(ENV['CF_CONTAINER'])
-    container.objects().map { |obj| container.object(obj) }
+    container.objects().map { |obj| "#{container.cdn_url}/#{obj}" }
   end
 end
 
